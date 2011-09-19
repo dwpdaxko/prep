f610c349d5f437e67c2e5eaee57153ab80d876c0
Jace Bennett <jace.bennett@gmail.com>
2011-09-19 13:28:31 -0400

    Pushing new changes

--- source/nothinbutdotnetprep.specs/MovieLibrarySpecs.cs
+++ source/nothinbutdotnetprep.specs/MovieLibrarySpecs.cs
@@ -7,6 +7,7 @@ using nothinbutdotnetprep.collections;
 using nothinbutdotnetprep.specs.utility;
 using System.Linq;
 using nothinbutdotnetprep.utility;
+using nothinbutdotnetprep.utility.filtering;
 
 /* The following set of Context/Specification pairs are in place to specify the functionality that you need to complete for the MovieLibrary class.
  * MovieLibrary is an collection of Movie. It exposes the ability to search,sort, and iterate over all of the movies that it contains.
---
+++ source/nothinbutdotnetprep/utility/filtering/Where.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nothinbutdotnetprep.utility.filtering
+{
+
+	public delegate object FieldSpec<TItem>(TItem item);
+
+	public static class Where<TItem>
+	{
+		public static Matcher<TItem> has_a(FieldSpec<TItem> spec)
+		{
+			return new Matcher<TItem>(spec);
+		}
+	}
+
+	public class Matcher<TItem>
+	{
+		private FieldSpec<TItem> left_side;
+
+		public Matcher(FieldSpec<TItem> value)
+		{
+			left_side = value;
+		}
+
+		public Condition<TItem> equal_to(object val)
+		{
+			return item => left_side(item) == val;
+		}
+	}
+}
