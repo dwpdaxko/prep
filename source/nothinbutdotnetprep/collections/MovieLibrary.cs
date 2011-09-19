ea346a520776216f872afb1cba117908d7d34e3c
Trent Kocurek <t2kocurek@gmail.com>
2011-09-19 10:41:28 -0500

    fixed finding in list tests

--- source/nothinbutdotnetprep/collections/MovieLibrary.cs
+++ source/nothinbutdotnetprep/collections/MovieLibrary.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace nothinbutdotnetprep.collections
@@ -22,30 +23,32 @@ namespace nothinbutdotnetprep.collections
 
         public void add(Movie movie)
         {
-            bool should_add = true;
-            foreach (var _movie in movies)
-            {
-                if (_movie.Equals(movie))
-                    should_add = false;
-            }
-
-            if (should_add)
+            if (!movies.Contains(movie))
                 movies.Add(movie);
         }
 
         public IEnumerable<Movie> sort_all_movies_by_title_descending()
         {
-            throw new NotImplementedException();
+            var sorted_movie_list = new List<Movie>(movies);
+            return sorted_movie_list;
         }
 
         public IEnumerable<Movie> all_movies_published_by_pixar()
         {
-            throw new NotImplementedException();
+            foreach (var movie in this.movies)
+            {
+                if (movie.production_studio == ProductionStudio.Pixar)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
         {
-            throw new NotImplementedException();
+            foreach (var movie in this.movies)
+            {
+                if (movie.production_studio == ProductionStudio.Pixar || movie.production_studio == ProductionStudio.Disney)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> sort_all_movies_by_title_ascending()
@@ -60,27 +63,47 @@ namespace nothinbutdotnetprep.collections
 
         public IEnumerable<Movie> all_movies_not_published_by_pixar()
         {
-            throw new NotImplementedException();
+            foreach (var movie in this.movies)
+            {
+                if (movie.production_studio != ProductionStudio.Pixar)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_movies_published_after(int year)
         {
-            throw new NotImplementedException();
+            foreach (var movie in this.movies)
+            {
+                if (movie.date_published.Year > year)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
         {
-            throw new NotImplementedException();
+            foreach (var movie in this.movies)
+            {
+                if (movie.date_published.Year >= startingYear && movie.date_published.Year <= endingYear)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_kid_movies()
         {
-            throw new NotImplementedException();
+            foreach (var movie in this.movies)
+            {
+                if (movie.genre == Genre.kids)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> all_action_movies()
         {
-            throw new NotImplementedException();
+            foreach (var movie in this.movies)
+            {
+                if (movie.genre == Genre.action)
+                    yield return movie;
+            }
         }
 
         public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
@@ -91,18 +114,6 @@ namespace nothinbutdotnetprep.collections
         public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
         {
             throw new NotImplementedException();
-        }
-
-
-        public IEnumerable<Movie> get_movies_by(ISpecification<Movie> specification)
-        {
-            var movie_list = new List<Movie>();
-            foreach (var movie in movies)
-            {
-                if (specification.IsSatisfiedBy(movie))
-                    movie_list.Add(movie);
-            }
-            return movie_list;
         }
     }
 }
